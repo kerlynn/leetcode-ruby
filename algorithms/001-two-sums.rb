a96d711bf9998e33f5dfb01e3fc0b8b53065fbb3
Kerlynn <43892411+kerlynn@users.noreply.github.com>
2022-06-16 22:30:45 +0800

    style:rename

--- algorithms/001-two-sums.rb
+++
@@ -1,7 +0,0 @@
-def two_sum(nums, target)
-  nums.each_with_index do |num,index|
-      value = target - num
-      value_index = nums.find_index(value)
-      return [index,value_index] if value_index  && value_index != index
-  end 
-end
